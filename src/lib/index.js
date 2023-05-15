d27731cb23d90438c0b3bfb1e48cd82e06116ad4
JohnnyRuss <georgiaspontoeli123@gmail.com>
2023-05-15 21:28:24 +0400

    add demo json files

---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+export {default as products} from "./products.json"
+export {default as developedProducts} from "./developedProducts.json"
+export {default as variants} from "./variants.json"
+export {default as colors} from "./colors.json"
